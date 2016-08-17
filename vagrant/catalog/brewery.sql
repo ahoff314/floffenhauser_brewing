c2459d01470dfbf9776c075eb262a3528b9965f7
Alex Hoffmann <alex@castalia.io>
2016-08-16 19:58:33 -0600

    First brew commit

---
+++ vagrant/catalog/brewery.sql
@@ -0,0 +1,8 @@
+-- Denver Brewing Festival - Microbreweries and Home Brewers
+
+CREATE TABLE beers (id SERIAL,
+                    name TEXT,
+                    style TEXT);
+
+
+CREATE VIEW
---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,40 @@
+import sys
+import sqlalchemy
+import sqlalchemy.ext.declarative
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Brewery(Base):
+    __tablename__ = 'breweries'
+
+    name = Column(
+        String(80), nullable=False)
+    id = Column(
+        Integer, primary_key=True)
+
+
+class Beer(Base):
+    __tablename__ = 'beers'
+
+    name = Column(
+        String(80), nullable=False)
+    id = Column(
+        Integer, primary_key=True)
+    style = Column(
+        String(80))
+    brewery = Column(
+        Integer, ForeignKey('breweries.id'))
+
+
+#### INSERT at end of file ####
+engine = create_engine(
+    'sqlite:///brews.db'
+)
+
+Base.metadata.create_all(engine)
