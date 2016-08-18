14ab0fc60e585a3d08158e9ddf997bda39f72c70
Alex Hoffmann <alex@castalia.io>
2016-08-18 17:45:10 -0600

    Second brew commit

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,18 +1,23 @@
 from flask import Flask, redirect, render_template
-#from flask_bootstrap import Bootstrap
+
+# from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-#bootstrap = Bootstrap(app)
 
+
+# bootstrap = Bootstrap(app)
+
+@app.route('/index')
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/about')
 def market():
     return render_template('about.html')
 
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-
