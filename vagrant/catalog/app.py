bf849fccd7e7bc5665411da9da214a0a6df8c0e8
Alex Hoffmann <alex@castalia.io>
2016-08-24 20:58:49 -0600

    Styling and event banner

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup  import Base, Brewery, Beer
+from database_setup import Base, Brewery, Beer
 
 engine = create_engine('sqlite:///brew.db')
 Base.metadata.bind = engine
@@ -11,30 +14,32 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# HOME PAGE
 @app.route('/')
 def home():
     brewery = session.query(Brewery).all()
     return render_template('index.html', brewery=brewery)
 
+# BREWERY INFO PAGE
 @app.route('/breweries/<int:brewery_id>/')
 def index(brewery_id):
-    brewery = session.query(Brewery).first() # SHould we use dot first??
+    brewery = session.query(Brewery).first()
     beers = session.query(Beer).filter_by(brewery_id=brewery.id)
     return render_template('beers.html', brewery=brewery, beers=beers)
 
-# Create route for new beer CLEAN UP ROUTE NAMES  intuitive
-
+# NEW BEER ROUTE
 @app.route('/breweries/<int:brewery_id>/new/', methods=['GET', 'POST'])
 def newBeer(brewery_id):
     if request.method == 'POST':
         newBeer = Beer(name=request.form['name'], brewery_id=brewery_id)
         session.add(newBeer)
         session.commit()
+        flash('New beer has been added. Cheers!')
         return redirect(url_for('index', brewery_id=brewery_id))
     else:
         return render_template('newbeer.html', brewery_id=brewery_id)
 
-# Create route for edit beers
+# EDIT BEER ROUTE
 @app.route('/breweries/<int:brewery_id>/<int:id>/edit/', methods=['GET', 'POST'])
 def editBeer(brewery_id, id):
     editedItem = session.query(Beer).filter_by(id=id).one()
@@ -43,18 +48,19 @@ def editBeer(brewery_id, id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash("This beer has been successfully edited. Cheers!")
         return redirect(url_for('index', brewery_id=brewery_id))
     else:
         return render_template('editbeer.html', brewery_id=brewery_id, i=editedItem)
 
-#create route for delete beers
-
+# DELETE BEER ROUTE
 @app.route('/breweries/<int:brewery_id>/<int:id>/delete/', methods=['GET', 'POST'])
 def deleteBeer(brewery_id, id):
     itemToDelete = session.query(Beer).filter_by(id=id).one()
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        flash("This beer has been removed.")
         return redirect(url_for('index', brewery_id = brewery_id))
     else:
         return render_template('deletebeer.html', i = itemToDelete)
@@ -66,5 +72,6 @@ def about():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret'
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
