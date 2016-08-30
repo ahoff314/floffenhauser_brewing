b4d5d799f2010bc53e7c37feed472da767da0f9e
Alex Hoffmann <alex@castalia.io>
2016-08-29 18:02:12 -0600

    500 error

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -305,8 +305,12 @@ def about():
 def page_not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret'
     app.debug = True
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
